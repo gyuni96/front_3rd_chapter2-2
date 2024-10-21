06878635e073785c0f5c4cb83bfa2d68f38cdff0
남균 <gyuni9604@naver.com>
2024-10-21 22:09:00 +0900

    ✨ feat : useProduct, useCoupon 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,17 @@
-import { useState } from 'react';
-import { Product } from '../../types.ts';
+import { useState } from "react"
+import { Product } from "../../types.ts"
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
-};
+  const [products, setProducts] = useState<Product[]>(initialProducts)
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prev) =>
+      prev.map((item) => (item.id === updatedProduct.id ? updatedProduct : item))
+    )
+  }
+  const addProduct = (addProduct: Product) => {
+    setProducts((prev) => [...prev, addProduct])
+  }
+
+  return { products, updateProduct, addProduct }
+}
